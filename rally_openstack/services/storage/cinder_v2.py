e9188bfc9ed99d752950f26ed86f17f4e7a89324
DheerajDang <dang@voereir.com>
2020-01-06 14:03:16 +0530

    removing unsupported multiattach keyword [Delivers #170540286]

--- rally_openstack/services/storage/cinder_v2.py
+++ rally_openstack/services/storage/cinder_v2.py
@@ -33,7 +33,7 @@ class CinderV2Service(service.Service, cinder_common.CinderMixin):
                       snapshot_id=None, source_volid=None, name=None,
                       description=None, volume_type=None,
                       availability_zone=None, metadata=None, imageRef=None,
-                      scheduler_hints=None, multiattach=False):
+                      scheduler_hints=None):
         """Creates a volume.
 
         :param size: Size of volume in GB
@@ -62,8 +62,7 @@ class CinderV2Service(service.Service, cinder_common.CinderMixin):
                   "availability_zone": availability_zone,
                   "metadata": metadata,
                   "imageRef": imageRef,
-                  "scheduler_hints": scheduler_hints,
-                  "multiattach": multiattach}
+                  "scheduler_hints": scheduler_hints}
         if isinstance(size, dict):
             size = random.randint(size["min"], size["max"])
 
